73f9705400b6e372eea028a9b402846bfdd9c309
kjh01140 <kjh011402023@gmail.com>
2024-09-26 01:05:31 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240926/높은 수에서 낮은 수까지/from-high-to-low.c
+++ 240926/높은 수에서 낮은 수까지/from-high-to-low.c
@@ -3,8 +3,15 @@
 int main() {
     int a,b;
     scanf("%d %d",&a,&b);
-    for(int i=b;i>=a;i--){
+    if(a<b){
+        for(int i=b;i>=a;i--){
         printf("%d ",i);
+        }
+    } else{
+        for(int i=a;i>=b;i--){
+        printf("%d ",i);
+        }
+
     }
     // 여기에 코드를 작성해주세요.
     return 0;
