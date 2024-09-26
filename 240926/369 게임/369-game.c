4d9e45a75c2ac7a6cb9743554a5caf8ea619ad9c
kjh01140 <kjh011402023@gmail.com>
2024-09-26 21:09:34 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240926/369 게임/369-game.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+for(int i=1; i<=n;i++){
+
+    if(i>=30&&i<=39 || i>=60&&i<=69 || i>=90&&i<=99){
+        printf("%d ",0);
+    }
+
+
+    if( i>=1&&i<10 ){
+        if(i%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }
+    if( i>=10&&i<20 ){
+        if((i-10)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }
+    if( i>=20&&i<30 ){
+        if((i-20)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }if( i>=40&&i<50 ){
+        if((i-40)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }if( i>=50&&i<60 ){
+        if((i-50)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }
+    if( i>=70&&i<80 ){
+        if((i-70)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }
+    if( i>=80&&i<90 ){
+        if((i-80)%3==0){
+            printf("%d ",0);
+        } else{printf("%d ",i);}
+    }
+}
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
