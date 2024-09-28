6e599de21fc4d3e20797ccd7a06130c4a7d73502
kjh01140 <kjh011402023@gmail.com>
2024-09-28 21:46:18 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240928/a부터 b까지의 합/sum-from-a-to-b.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,sum=0;
+    scanf("%d %d",&a,&b);
+    for(int i=a;i<=b;i++){
+        sum+=i;
+    }
+    printf("%d",sum);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
