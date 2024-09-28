472810f269f98a7b93cac2b61ffcf1bc1e149aa2
kjh01140 <kjh011402023@gmail.com>
2024-09-28 13:08:02 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240928/배수의 개수/number-of-multipliers.c
+++ 240928/배수의 개수/number-of-multipliers.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int cnt=0,pnt=0,n;
+    int cnt1=0,cnt2=0;
     for(int i=0;i<10;i++){
+        int n;
         scanf("%d",&n);
-        if(n%3==0){
-            cnt++;
-        }
-        if(n%5==0){
-            pnt++;
-        }
+        if(n%3==0)
+            cnt1++;
+        if(n%5==0)
+            cnt2++;
     }
-    printf("%d %d",cnt,pnt);
+    printf("%d %d",cnt1,cnt2);
     // 여기에 코드를 작성해주세요.
     return 0;
 }
