b772e852e8f2059b66347e5a4f4fdbaf9717e7b6
kjh01140 <kjh011402023@gmail.com>
2024-09-29 18:38:25 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/3n + 1 수열/3n-plus-1-sequence.c
+++ 240929/3n + 1 수열/3n-plus-1-sequence.c
@@ -4,6 +4,10 @@ int main() {
     int N,cnt=0;
     scanf("%d",&N);
     for(;;){
+        if(N==1){
+            printf("%d",cnt);
+            break;
+        }
         if(N%2==0){
             N/=2;
             cnt++;
@@ -11,10 +15,7 @@ int main() {
             N=N*3+1;
             cnt++;
         }
-        if(N==1){
-            printf("%d",cnt);
-            break;
-        }
+        
     }
     // 여기에 코드를 작성해주세요.
     return 0;
