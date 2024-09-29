c7700fab2ca30383237a6896e968ad0c5ec76210
kjh01140 <kjh011402023@gmail.com>
2024-09-29 20:57:11 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/ab 사이에 없는 c/c-between-a-and-b-2.c
+++ 240929/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -3,13 +3,13 @@
 int main() {
     int a,b,c;
     bool s=true;
-    scanf("%d %d %c",&a,&b,&c);
+    scanf("%d %d %d",&a,&b,&c);
     for(int i=a; i<=b;i++){
         if (i%c==0){
             s=false;
         }
     }
-    printf(s==true?"YES":"NO");
+    printf(s?"YES":"NO");
     // 여기에 코드를 작성해주세요.
     return 0;
 }
