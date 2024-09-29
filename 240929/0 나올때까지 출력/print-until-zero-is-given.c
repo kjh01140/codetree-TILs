a54f4d183d2041401a5b3978ff509899fef9ff69
kjh01140 <kjh011402023@gmail.com>
2024-09-29 17:59:49 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240929/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    for(;;){
+        int n;
+        scanf("%d",&n);
+        if(n==0)
+            break;  
+        printf("%d\n",n);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
