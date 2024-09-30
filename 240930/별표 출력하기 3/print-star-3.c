4299db1d81e48907fcf4f6fd37da95c8c4a2ea9a
kjh01140 <kjh011402023@gmail.com>
2024-09-30 12:05:31 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240930/별표 출력하기 3/print-star-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    for(int j=0;j<n;j++){
+
+        for(int i=0;i<j;i++){
+        printf("  ");
+        }
+
+        for(int i=(n-j)*2-1;i>=1;i--){
+        printf("* ");
+        }
+        printf("\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
