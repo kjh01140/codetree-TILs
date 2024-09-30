784644aaa147dc8437dec5388b5c63f90757770a
kjh01140 <kjh011402023@gmail.com>
2024-09-30 11:49:04 +0900

    Title: 건너뛰고 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240930/건너뛰고 별표 출력하기/skip-and-print-a-star.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1 ;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int j=1 ;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+        printf("\n");
+    }
+
+
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
