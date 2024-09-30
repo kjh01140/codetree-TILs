87c7a676a9d256325aa8b37ead985740ced8d775
kjh01140 <kjh011402023@gmail.com>
2024-09-30 12:16:32 +0900

    Title: 별표 출력하기 9, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240930/별표 출력하기 9/print-star-9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=n-2-i;j>=0;j--){
+            printf("  ");
+        }
+        for(int j=1;j<=2*i+1;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
