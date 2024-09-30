97e4be1a465c14d2561b7ce8b92c71739625d599
kjh01140 <kjh011402023@gmail.com>
2024-09-30 22:33:36 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240930/규칙을 찾아 출력하기/find-a-rule-and-print.c
+++ 240930/규칙을 찾아 출력하기/find-a-rule-and-print.c
@@ -8,13 +8,13 @@ int main() {
     
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(i==0||i==n||j==0||j==n){
+            if(i==0||i==n-1||j==0||j==n-1){
                 printf("* ");
             } else if(i>=j){
                 printf("* ");
             } else{
                 printf("  ");
-                
+
             }
         }
         printf("\n");
