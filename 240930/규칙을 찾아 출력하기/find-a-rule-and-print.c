8cf6d4703e95a9301c5f2453ff14fa6e49682f43
kjh01140 <kjh011402023@gmail.com>
2024-09-30 22:34:14 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240930/규칙을 찾아 출력하기/find-a-rule-and-print.c
+++ 240930/규칙을 찾아 출력하기/find-a-rule-and-print.c
@@ -10,7 +10,7 @@ int main() {
         for(int j = 0; j < n; j++) {
             if(i==0||i==n-1||j==0||j==n-1){
                 printf("* ");
-            } else if(i>=j){
+            } else if(i>j){
                 printf("* ");
             } else{
                 printf("  ");
