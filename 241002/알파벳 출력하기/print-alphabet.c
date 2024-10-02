e7fe7a33e196a3f205600a2fb105b73ebbc7f92a
kjh01140 <kjh011402023@gmail.com>
2024-10-02 21:05:45 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/알파벳 출력하기/print-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+	char cnt = 'A';
+	
+	// 알파벳을 정사각형 모양으로 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < i+1; j++) {
+			printf("%c", cnt);
+			cnt++;
+		}
+		printf("\n");
+	}
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
