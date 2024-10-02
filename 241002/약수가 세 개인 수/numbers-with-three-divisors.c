551e2f0c8c65dd19cab46f6f5b1642e31ec6be24
kjh01140 <kjh011402023@gmail.com>
2024-10-02 23:44:51 +0900

    Title: 약수가 세 개인 수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241002/약수가 세 개인 수/numbers-with-three-divisors.c
+++ 241002/약수가 세 개인 수/numbers-with-three-divisors.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int start,end,cnt=0,result=0;
+    int start,end,result=0;
     scanf("%d %d",&start,&end);
 
     for(int i=start;i<=end;i++){ //숫자선정
-
+    int cnt=0;
         for(int j=1;j<i;j++){    //약수 돌림
             if(i%j==0){
                 cnt++;
