65cebd1edbc59755d58f6668b046ace7d999ec55
kjh01140 <kjh011402023@gmail.com>
2024-10-03 16:30:03 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/학점 계산기/credit-calculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    double arr[n],sum=0.0,average=0.0;
+
+    for(int i=0;i<n;i++){
+        scanf("%lf",&arr[i]);
+        sum+=arr[i];
+
+    }
+
+    average=sum/n;
+    if(average>=4.0){
+        printf("%.1lf \nPerfect",average);
+    } else if(average>=3.0){
+        printf("%.1lf \nGood",average);
+    } else{
+        printf("%.1lf \nPoor",average);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
