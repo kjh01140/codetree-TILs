8fa082f670b4d0d664dfbb63049db4df25fc6902
kjh01140 <kjh011402023@gmail.com>
2024-10-03 18:16:31 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/특정 위치의 숫자/numbers-in-specific-location.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    for(int i=0;i<10;i++){
+        scanf("%d",&arr[i]);
+
+    }
+    printf("%d",arr[2]+arr[4]+arr[9]);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
