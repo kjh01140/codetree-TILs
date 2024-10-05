97e8dad33c86737cb03e2de298e5cfd24df0702f
kjh01140 <kjh011402023@gmail.com>
2024-10-05 22:05:55 +0900

    Title: 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/개수 세기/count-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n,m,cnt=0;
+    scanf("%d %d",&n,&m);
+    int arr[100];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]==m){
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
