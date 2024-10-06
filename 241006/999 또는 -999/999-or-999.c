db97077248be66b50dfab27a08b13489464117dc
kjh01140 <kjh011402023@gmail.com>
2024-10-06 16:22:28 +0900

    Title: 999 또는 -999, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/999 또는 -999/999-or-999.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int main() {
+    int arr[100], max=INT_MIN, min=INT_MAX;
+    for(int i=0;i<100;i++){
+        scanf("%d",&arr[i]);
+
+        if(arr[i]==999||arr[i]==999){
+            break;
+        }
+       if(arr[i]>max) max=arr[i];
+       if(arr[i]<min) min=arr[i];
+
+    }
+    printf("%d %d",max,min);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
