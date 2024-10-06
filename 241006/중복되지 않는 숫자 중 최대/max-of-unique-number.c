03f479c30e6420aae236367a9759cfe3856c2a2c
kjh01140 <kjh011402023@gmail.com>
2024-10-06 16:52:50 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241006/중복되지 않는 숫자 중 최대/max-of-unique-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int N,max=-1;
+    scanf("%d",&N);
+    int arr[N],cnt_arr[1000]={0,};
+
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
+        cnt_arr[arr[i]]++;
+    }
+
+    for(int i=1000;i>=0;i--){
+        if(cnt_arr[i]!=1){
+            continue;
+        } else{
+            max=i;
+        }
+    }
+    printf("%d",max);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
