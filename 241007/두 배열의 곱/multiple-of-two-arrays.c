1595b6c1869de1931290202965a6843a556fe885
kjh01140 <kjh011402023@gmail.com>
2024-10-07 18:07:42 +0900

    Title: 두 배열의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/두 배열의 곱/multiple-of-two-arrays.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[3][3],arr2[3][3];
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&arr1[i][j]);
+        }
+    }  
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&arr2[i][j]);
+        }
+    }  
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",arr1[i][j]*arr2[i][j]);
+        }
+        printf("\n");
+    }  
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
