dd4fefb3b367902589016e9a9bb30592e41badbd
kjh01140 <kjh011402023@gmail.com>
2024-10-09 10:24:01 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241009/가장 짧은 문자열/shortest-string.c
+++ 241009/가장 짧은 문자열/shortest-string.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    int str1[20],str2[20],str3[20],arr[3];
-    scanf("%s",str1);
-    scanf("%s",str2);
-    scanf("%s",str3);
+    char str1[20], str2[20], str3[20];
+    scanf("%s", str1);
+    scanf("%s", str2);
+    scanf("%s", str3);
 
+    // 문자열 길이를 저장할 변수
+    int max = (int)strlen(str1);
+    int min = (int)strlen(str1);
+
+    // 각 문자열의 길이를 비교하여 최대 및 최소 길이 찾기
+    int lengths[3] = {strlen(str1), strlen(str2), strlen(str3)};
     
-    int a=(int)strlen(str1), b=(int)strlen(str2), c=(int)strlen(str3);
-    int max=a,min=a;
-    arr={a,b,c};
-
-    for(int i=1;i<3;i++){
-        if(max<arr[i]){
-            max=arr[i]
+    for (int i = 1; i < 3; i++) {
+        if (max < lengths[i]) {
+            max = lengths[i];
         }
-        if(min>arr[i]){
-            min=arr[i];
+        if (min > lengths[i]) {
+            min = lengths[i];
         }
     }
-    printf("%d",max-min);
-    
-    // 여기에 코드를 작성해주세요.
+
+    // 최대 길이와 최소 길이의 차이를 출력
+    printf("%d\n", max - min);
     return 0;
 }
