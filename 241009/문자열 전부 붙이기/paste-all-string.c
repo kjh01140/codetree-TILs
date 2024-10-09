c9117754cd462884d9c92797811d3d0861adeb86
kjh01140 <kjh011402023@gmail.com>
2024-10-09 20:25:32 +0900

    Title: 문자열 전부 붙이기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241009/문자열 전부 붙이기/paste-all-string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    char arr[20];
+    scanf("%s",arr);
+
+    for(int i=1;i<n;i++){
+        char n_arr[20];
+        scanf("%s",n_arr);
+        strcat(arr,n_arr);
+
+    }
+    printf("%s",arr);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
