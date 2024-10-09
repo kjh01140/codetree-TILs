1b3f67d0fb59265e82c254be453101392d07de5e
kjh01140 <kjh011402023@gmail.com>
2024-10-09 09:31:47 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241009/더 긴 문자열/longer-string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+#include <string.h>
+
+int main() {
+    char str1[100],str2[100];
+
+    scanf("%s",str1);
+    scanf("%s",str2);
+    //printf("%s %d", strlen(str1)>strlen(str2) ? str1,strlen(str1):str2,strlen(str2));
+    if( strlen(str1)>strlen(str2)){
+        printf("%s %d",str1,strlen(str1));
+    } else{
+        printf("%s %d",str2,strlen(str2));
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
