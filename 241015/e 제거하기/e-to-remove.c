e3dbe705d40b6325ddc9f875d0742e288582c8f6
kjh01140 <kjh011402023@gmail.com>
2024-10-15 21:50:23 +0900

    Title: e 제거하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/e 제거하기/e-to-remove.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[101];
+    scanf("%s",&s);
+    int len= strlen(s);
+
+    for(int i=0;i<len;i++){
+        if (s[i]=='e'){
+            for(int j=i;j<len-1;j++){
+                s[j]=s[j+1];
+            }
+
+        }
+        
+    }
+    len--;
+    s[len]='\0';
+
+    printf("%s",s); 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
