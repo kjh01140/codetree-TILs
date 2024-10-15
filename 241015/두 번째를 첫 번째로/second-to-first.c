147dbc0fc9939cf6ae907fc35dca34ff35fba79d
kjh01140 <kjh011402023@gmail.com>
2024-10-15 20:11:33 +0900

    Title: 두 번째를 첫 번째로, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241015/두 번째를 첫 번째로/second-to-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[20];
+    scanf("%s",str);
+    int len=strlen(str);
+
+    char first=str[0],second=str[1];
+
+    for(int i=0;i<len;i++){
+        if (str[i]==second){
+            str[i]=first;
+        }
+    }
+
+    printf("%s",str);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
