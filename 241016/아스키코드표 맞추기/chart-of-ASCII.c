5570ab9a684e368caf3af639820657d007c8085e
kjh01140 <kjh011402023@gmail.com>
2024-10-16 13:24:07 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241016/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+
+    for(int i=0;i<5;i++){
+        int n;
+        scanf("%d",&n);
+        printf("%c ",n);
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
