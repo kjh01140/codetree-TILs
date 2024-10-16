536f9670559acadea220fd08a54aa6e3a1ecde3a
kjh01140 <kjh011402023@gmail.com>
2024-10-16 13:35:53 +0900

    Title: 그 전 알파벳, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241016/그 전 알파벳/before-the-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c",&a);
+
+    if(a=='a'){
+        printf("%c",'z');
+
+    } else{
+        printf("%c",a-1);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
