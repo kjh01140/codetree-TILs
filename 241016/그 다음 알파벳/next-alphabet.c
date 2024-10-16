c0ce5c463d9fad59fd168a8224cbcb594b52cc74
kjh01140 <kjh011402023@gmail.com>
2024-10-16 13:29:12 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241016/그 다음 알파벳/next-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c",&a);
+
+    if(a=='z'){
+        printf("%c",'a');
+
+    } else{
+        printf("%c",a+1);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
