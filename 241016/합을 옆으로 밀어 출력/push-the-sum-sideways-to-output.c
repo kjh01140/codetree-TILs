3dc6fb53704651e17e9cb3b7cfe2c166c3c3cf74
kjh01140 <kjh011402023@gmail.com>
2024-10-16 22:26:11 +0900

    Title: 합을 옆으로 밀어 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241016/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n,sum=0;
+    char str[10000];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int num;
+        scanf("%d",&num);
+        sum+=num;
+    }
+    sprintf(str,"%d",sum);
+
+    int len=strlen(str),temp=str[0];
+
+    for(int i=1;i<len-1;i++){
+        str[i-1]=str[i];
+    }
+    str[len-1]=temp;
+    printf("%s",str);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
