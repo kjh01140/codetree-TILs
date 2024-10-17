b74a6c0b93f737f44ff4a0ddcf2619c920e92bf4
kjh01140 <kjh011402023@gmail.com>
2024-10-17 10:03:55 +0900

    Title: 미는 횟수, Time: 1096ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 241017/미는 횟수/number-of-pushes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[101],s2[101];
+    scanf("%s",s1);
+    scanf("%s",s2);
+    int cnt=0, len=strlen(s1);
+    
+    
+    for(;;){
+        char temp= s2[len-1];
+        cnt++;
+        for(int i=len-1;i>0;i-- ){
+        s2[i]=s2[i-1];
+        }
+
+        s2[0]=temp;
+        if(strcmp(s1,s2)==0)break;
+    }
+    printf("%d",cnt);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
