e38637258d6d0b4e23473fe915ebd9ff6e2f9c34
kjh01140 <kjh011402023@gmail.com>
2024-10-23 17:48:24 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "Total days in Year"<<"\n"<<365<<"\n"<<"Circumference rate"<<"\n";
+    cout<<3.14159<<26535;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
