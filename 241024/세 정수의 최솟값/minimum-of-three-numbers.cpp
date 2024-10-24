8b6f6e4851ee444ba206af3dc0d11dd78338df30
kjh01140 <kjh011402023@gmail.com>
2024-10-24 19:57:55 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 241024/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -6,7 +6,11 @@ int main() {
     cin >> a >> b >> c;
 
     if(a<=b){
-        if(a<=c) cout << a;
+        if(a<=c) {
+            cout << a;
+        } else{
+            cout << c;
+        }
 
     } else if (b <= c){
         cout << b;
