d5ec027640a2ac98ec11975f63a53e450e2aa83c
kjh01140 <kjh011402023@gmail.com>
2024-10-24 20:08:35 +0900

    Title: 두 사람, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241024/두 사람/two-person.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, a2;
+    char g1, g2;
+    cin >> a1 >> g1 >> a2 >> g2;
+
+    cout << (a1>=19 && g1 == 'M' || a2>=19 && g2 == 'M');
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
