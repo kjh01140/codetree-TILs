4f02af5194688f2520350625e4d0c45f41daa8d4
kjh01140 <kjh011402023@gmail.com>
2024-10-25 17:26:16 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241025/369 게임/369-game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        if( i%10==3 || i%10==6 || i%10==9 || i%3==0){
+            cout << "0" << " ";
+        }else{
+            cout << i << " ";
+        }
+    } 
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
