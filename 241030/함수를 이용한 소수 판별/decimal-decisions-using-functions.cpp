3ce889c4df6a663141c361492870ec6944ff0235
kjh01140 <kjh011402023@gmail.com>
2024-10-30 12:58:50 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241030/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 bool Func (int n){
+    if (n==1) return false;
     for(int i=2;i<n;i++){
         if (n%i==0) return false;
     }
@@ -10,7 +11,7 @@ bool Func (int n){
 int main() {
     int a,b,sum=0;
     cin >> a>>b;
-
+    
     for(int i=a;i<=b;i++){
         if(Func(i)) sum+=i;
     }
