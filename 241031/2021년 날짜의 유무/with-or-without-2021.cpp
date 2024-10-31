ab8c18ff96a959f59968c7b40b5630672d193429
kjh01140 <kjh011402023@gmail.com>
2024-10-31 14:40:06 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241031/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+bool Func(int M, int D){
+    if(M<8){
+        if(M==2){
+            if(D<=28) return true;
+
+        } else if(M%2==0){
+            if(D<=30) return true;
+
+        }else{
+            if(D<=31) return true;
+
+        }
+    }else{
+        if(M%2==0){
+            if(D<=31) return true;
+
+        }else{
+            if(D<=30) return true;
+
+
+    } 
+    return false;
+    }
+}
+
+int main() {
+    int M,D;
+    cin >> M >> D;
+if(Func(M,D)){
+    cout << "Yes";
+} else {
+    cout << "No";
+}
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
