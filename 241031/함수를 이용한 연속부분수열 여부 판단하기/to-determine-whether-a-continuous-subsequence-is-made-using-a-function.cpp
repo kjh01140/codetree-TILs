e63b45773360f779e84ee9f39e303b0ace81efd2
kjh01140 <kjh011402023@gmail.com>
2024-10-31 14:26:36 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 241031/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -2,43 +2,39 @@
 #include <string>
 using namespace std;
 
-bool Func(string A, string B){
-    if(A.find(B) == string::npos){
-        return false;
-    } else{
-        return true;
+bool Func(int A[], int B[], int n1, int n2) {
+    if (n2 > n1) return false; // 부분 배열이 더 길면 바로 false 반환
+
+    for (int i = 0; i <= n1 - n2; i++) {
+        bool found = true;
+        for (int j = 0; j < n2; j++) {
+            if (A[i + j] != B[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) return true;
     }
+    return false;
 }
 
 int main() {
-    int n1,n2;
+    int n1, n2;
     cin >> n1 >> n2;
-    int A[n1],B[n2];
+    int A[n1], B[n2];
 
-    for(int i=0; i<n1; i++ ){
+    for (int i = 0; i < n1; i++) {
         cin >> A[i];
     }
-    for(int i=0; i<n2; i++ ){
+    for (int i = 0; i < n2; i++) {
         cin >> B[i];
     }
 
-// 배열 A와 B를 문자열로 변환
-    string strA = "", strB = "";
-    for (int i = 0; i < n1; i++) {
-        strA += to_string(A[i]);
-    }
-    for (int i = 0; i < n2; i++) {
-        strB += to_string(B[i]);
-    }
-
-
-
-    if(Func(strA,strB)){
+    if (Func(A, B, n1, n2)) {
         cout << "Yes";
-    } else{
+    } else {
         cout << "No";
     }
 
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
