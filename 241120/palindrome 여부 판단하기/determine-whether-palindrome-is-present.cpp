5d9bf020b87425267ebb99edde4997707e5da401
kjh01140 <kjh011402023@gmail.com>
2024-11-20 21:25:00 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241120/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 241120/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -2,33 +2,23 @@
 #include <string>
 using namespace std;
 
-// 회문 여부를 확인하는 함수 (수정 없음)
-bool CheckP(string *s, int length) {
-    for (int i = 0; i < length; i++) {
-        if (*(s + i) == *(s + length - 1 - i)) {
-            continue;
-        } else {
+bool CheckP(const string &s) {
+    int length = s.length();
+    for (int i = 0; i < length / 2; i++) {
+        if (s[i] != s[length - 1 - i]) {
             return false;
         }
     }
     return true;
 }
 
+
 int main() {
-    string input;    // 전체 입력 문자열
+    string input;
     
-    cin >> input;    // 공백 없이 한 줄 입력
+    cin >> input;
 
-    string arr[100]; // 문자 배열로 변환
-    int length = input.length(); // 입력 문자열 길이
-
-    // 문자열을 문자 단위로 배열에 저장
-    for (int i = 0; i < length; i++) {
-        arr[i] = input[i];
-    }
-
-    // CheckP 함수 호출
-    if (CheckP(arr, length)) {
+    if (CheckP(input)) {
         cout << "Yes";
     } else {
         cout << "No";
