f4d481f6b57f3a37bbe1aacc85ca0dd14f04bb6c
kjh01140 <kjh011402023@gmail.com>
2024-11-27 18:43:56 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241127/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int cnt=0;
+int Func(int n){
+    if (n==1) return cnt;
+    if(n%2==0) {
+        cnt++; 
+        return Func(n/2);
+    } else{
+        cnt++;
+        return Func(n*3+1);
+    }
+
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << Func(n);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
