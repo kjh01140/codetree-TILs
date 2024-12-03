f11c30c82c24a36ecc2733950b44c222af4aeb8e
kjh01140 <kjh011402023@gmail.com>
2024-12-03 20:03:20 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241203/개인정보/personal-info.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Student {
+    public:
+        string name;
+        int height;
+        float weight;
+
+        Student (string name,int height,float weight){
+            this-> name = name;
+            this-> height = height;
+            this-> weight = weight;
+        }
+        Student(){}
+};
+bool cmp_h(Student a, Student b){
+    return a.name < b.name;
+}
+bool cmp_w(Student a, Student b){
+    return a.height > b.height;
+}
+
+int main() {
+    string name;
+    int height;
+    float weight;
+    
+    Student students[5];
+    for(int i=0; i<5; i++){
+        cin >> name >> height >> weight;
+        students[i].name= name;
+        students[i].height= height;
+        students[i].weight= weight;
+    }
+
+    sort(students, students+5, cmp_h);
+    cout << "name" << endl;
+    for(int i=0; i<5; i++){
+        cout << students[i].name << " ";
+        cout << students[i].height << " ";
+        cout << students[i].weight << endl;
+    }
+    cout << endl;
+
+    sort(students, students+5, cmp_w);
+    cout << "height" << endl;
+    for(int i=0; i<5; i++){
+        cout << students[i].name << " ";
+        cout << students[i].height << " ";
+        cout << students[i].weight << endl;
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
