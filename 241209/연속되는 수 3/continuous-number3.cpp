dd9a71faa67bfc0f79c2ebae66f6280159809b9c
kjh01140 <kjh011402023@gmail.com>
2024-12-09 20:57:38 +0900

    Title: 연속되는 수 3, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241209/연속되는 수 3/continuous-number3.cpp
+++ 241209/연속되는 수 3/continuous-number3.cpp
@@ -24,8 +24,12 @@ int main() {
 
         ans = max(ans, cnt);
     }
-
-    cout << ans;
+    if(ans == 1){
+        cout << 0;
+    } else{
+        cout << ans;
+    }
+    
     // 여기에 코드를 작성해주세요.
     return 0;
 }
