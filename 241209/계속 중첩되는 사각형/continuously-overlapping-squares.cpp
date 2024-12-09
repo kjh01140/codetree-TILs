8b3bf38f5a991602d591353ccc9d2dac5f373a93
kjh01140 <kjh011402023@gmail.com>
2024-12-09 11:10:31 +0900

    Title: 계속 중첩되는 사각형, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241209/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define OFFSET 100
+#define MaxC 201
+
+int main() {
+    int n; cin >> n;
+    int Cood[MaxC][MaxC] = {};
+    int cnt=0;
+
+    int color[2] = {1,2}; // Red, Blue
+
+    for(int i=0; i<n; i++){
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int j=x1; j<x2; j++ ){
+            for(int k=y1; k<y2; k++){
+                Cood[j][k]=color[cnt%2];
+                 
+            }
+        }
+        cnt++;
+
+    }
+
+    cout << count(&Cood[0][0], &Cood[0][0]+MaxC*MaxC, 2);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
