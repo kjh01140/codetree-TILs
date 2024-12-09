459dfb6c1fbf63bdf75cdc2d03674138a3117817
kjh01140 <kjh011402023@gmail.com>
2024-12-09 21:18:44 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241209/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX_N 1000
+
+using namespace std;
+
+int n,t;
+int arr[MAX_N];
+
+int main() {
+    // 입력
+    cin >> n >> t;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    // 연속하여 동일한 부호의 숫자가 나오는 횟수를 구해보며,
+    // 그 중 최댓값을 갱신합니다.
+    int ans = 0, cnt = 0;
+    for(int i = 0; i < n; i++) {
+		// Case 1
+        if(i >= 1 && arr[i]> t)
+            cnt++;
+		// Case 2
+        else
+            cnt = 1;
+        
+        ans = max(ans, cnt);
+    }
+    
+    cout << ans;
+    return 0;
+}
