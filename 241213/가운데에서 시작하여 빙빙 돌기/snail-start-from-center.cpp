4f6321107f915363bc45f5f419a434b73cd56293
kjh01140 <kjh011402023@gmail.com>
2024-12-13 19:46:47 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241213/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 241213/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -3,27 +3,26 @@ using namespace std;
 
 int n;
 int arr[102][102] = {};
-int num = 1;
+int num = 2; //두번째부터
 int x = 0, y = 0, dir = 0; // 방향 초기값: 오른쪽으로 시작
 
 // 방향: 오른쪽, 위, 왼쪽, 아래
 int dx[4] = {0, -1, 0, 1};
 int dy[4] = {1, 0, -1, 0};
 
-bool InRange(int x, int y) {
-    return 0 <= x && x < n && 0 <= y && y < n && arr[x][y] == 0;
-}
 
-void Move(int &x, int &y, int &dir, int &num) {
+
+void Move(int &x, int &y, int &dir, int &num) { //기본적으로 1부터는 방향전환 1순위 , 직진 2순위
     while (num <= n * n) {
         arr[x][y] = num++; // 현재 위치에 숫자 채우기
-
+        
+        dir = (dir + 1) % 4; //1순위는 방향변경후 한칸이동
         int nx = x + dx[dir];
         int ny = y + dy[dir];
-
-        // 범위를 벗어나거나 이미 채워진 경우 방향 변경
-        if (!InRange(nx, ny)) {
-            dir = (dir + 1) % 4; // 다음 방향으로 변경
+        
+        // 채워진 경우 전진
+        if (arr[nx][ny] != 0 ) {
+            dir = (dir+3)%4;
             nx = x + dx[dir];
             ny = y + dy[dir];
         }
@@ -37,6 +36,10 @@ int main() {
     cin >> n;
     x = (n - 1) / 2; // 시작 위치: 가운데
     y = (n - 1) / 2;
+    arr[x][y]= 1; //첫번째 1입력
+    y++;          //두번째 부터 시작
+    dir = 1;
+    
     Move(x, y, dir, num);
 
     for (int i = 0; i < n; i++) {
