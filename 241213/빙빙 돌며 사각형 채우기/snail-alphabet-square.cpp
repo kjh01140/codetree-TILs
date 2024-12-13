02f6b8297261ffe6f10409efb17237e40950b69f
kjh01140 <kjh011402023@gmail.com>
2024-12-13 18:14:30 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241213/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
+++ 241213/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int n, m;
-int arr[101][101] = {};
-int num = 1;
+char arr[101][101] = {}; // 알파벳을 저장할 배열
+int num = 0; // 현재 알파벳의 인덱스 (A: 0, ..., Z: 25)
 int x = 0, y = 0, dir = 0; // 방향 초기값: 오른쪽으로 시작
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
 
 bool InRange(int x, int y) {
     return 0 <= x && x < n && 0 <= y && y < m && arr[x][y] == 0;
 }
 
 void Move(int &x, int &y, int &dir, int &num) {
-    while (num <= n * m) {
-        arr[x][y] = num++; // 현재 위치에 숫자 채우기
-        if(num >26) num =1;
+    for (int i = 0; i < n * m; i++) {
+        arr[x][y] = 'A' + (num % 26); // 현재 위치에 알파벳 채우기
+        num++;
+
         int nx = x + dx[dir];
         int ny = y + dy[dir];
 
@@ -38,7 +39,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cout <<(char) (arr[i][j] + 'A'-1 )<< " ";
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
