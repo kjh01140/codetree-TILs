8b2965efea4bf462667eed68ee9d1c99f6f33428
kjh01140 <kjh011402023@gmail.com>
2024-12-14 12:39:56 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241214/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+char arr[16][16] = {};
+
+int cnt = 0;
+
+int main() {
+    int R, C; cin >> R >> C;
+
+    int x=0, y=0; //좌상단
+
+    for(int i=0; i<R; i++){ // 색칠
+        for(int j=0; j<C ;j++ ){
+            cin >> arr[i][j];    
+        }
+    }
+    char color = arr[0][0];  // 좌상단 색깔
+    
+    for(int i=x+1; i<R-1; i++){
+        for(int j=y+1; j<C-1; j++){
+            if(arr[i][j] != color ){
+                x = i; 
+                y = j;
+                color = arr[i][j];
+
+                for(int k=x+1; k<R-1; k++){
+                    for(int l=y+1; l<C-1; l++){
+                        if(arr[k][l] != color) cnt++;
+                    }
+                }
+
+            }
+        }
+    }
+    cout << cnt;
+
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
