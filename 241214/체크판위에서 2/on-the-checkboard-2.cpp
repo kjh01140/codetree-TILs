8e45e8c9d8c66244b5eccc601b38e5efbab5e511
kjh01140 <kjh011402023@gmail.com>
2024-12-14 13:04:00 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241214/체크판위에서 2/on-the-checkboard-2.cpp
+++ 241214/체크판위에서 2/on-the-checkboard-2.cpp
@@ -2,42 +2,42 @@
 using namespace std;
 
 char arr[16][16] = {};
-int result = 0;
+int R, C;
+int cnt = 0;
+
+void dfs(int x, int y, int jumps, char prev_color) {
+    // 현재 점프 횟수가 2를 초과하면 종료
+    if (jumps > 2) return;
+
+    // 도착 지점에 도달한 경우
+    if (x == R - 1 && y == C - 1) {
+        if (jumps == 2) cnt++;
+        return;
+    }
+
+    // 가능한 점프 탐색
+    for (int nx = x + 1; nx < R; nx++) {
+        for (int ny = y + 1; ny < C; ny++) {
+            if (arr[nx][ny] != prev_color) { // 색이 달라야 점프 가능
+                dfs(nx, ny, jumps + 1, arr[nx][ny]);
+            }
+        }
+    }
+}
 
 int main() {
-    int R, C;
     cin >> R >> C;
 
-    // 직사각형 색 정보 입력
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             cin >> arr[i][j];
         }
     }
 
-    // 첫 번째 점프 탐색
-    for (int i1 = 1; i1 < R - 1; i1++) {  // 첫 번째 점프의 행 (도착 지점 제외)
-        for (int j1 = 1; j1 < C - 1; j1++) {  // 첫 번째 점프의 열 (도착 지점 제외)
-            if (arr[i1][j1] != arr[0][0]) {  // 첫 번째 점프 조건: 색이 다름
+    // 시작점에서 탐색 시작
+    dfs(0, 0, 0, arr[0][0]);
 
-                // 두 번째 점프 탐색
-                for (int i2 = i1 + 1; i2 < R; i2++) {  // 두 번째 점프의 행
-                    for (int j2 = j1 + 1; j2 < C; j2++) {  // 두 번째 점프의 열
-                        if (arr[i2][j2] != arr[i1][j1]) {  // 두 번째 점프 조건: 색이 다름
-
-                            // 도착 지점 확인
-                            if (i2 == R - 1 && j2 == C - 1) {
-                                result++;  // 유효한 경로 카운트
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // 결과 출력
-    cout << result << endl;
+    cout << cnt << endl;
 
     return 0;
 }
