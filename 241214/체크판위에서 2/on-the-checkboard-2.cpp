72b39719670570f0842ccce8d50335a3607a6ec6
kjh01140 <kjh011402023@gmail.com>
2024-12-14 12:45:53 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241214/체크판위에서 2/on-the-checkboard-2.cpp
+++ 241214/체크판위에서 2/on-the-checkboard-2.cpp
@@ -17,10 +17,10 @@ int main() {
 
 
     char color = arr[0][0];  // 좌상단 색깔
-    if(color == arr[R-1][C-1]){ cout << 0; return 0;}
+    if(color == arr[R-1][C-1]){ cout << 0; return 0;} //필연적으로 불가능한 조건
     
-    for(int i=x+1; i<R-1; i++){
-        for(int j=y+1; j<C-1; j++){
+    for(int i=x+1; i<R-2; i++){
+        for(int j=y+1; j<C-2; j++){
             if(arr[i][j] != color ){
                 x = i; 
                 y = j;
@@ -28,7 +28,7 @@ int main() {
 
                 for(int k=x+1; k<R-1; k++){
                     for(int l=y+1; l<C-1; l++){
-                        if(arr[k][l] != color ) cnt++;
+                        if(arr[k][l] != color) cnt++;
                     }
                 }
 
