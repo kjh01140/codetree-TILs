bd8989b073d0299a572ad4ba4a59a05366963403
kjh01140 <kjh011402023@gmail.com>
2024-12-18 12:47:12 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241218/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 241218/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int arr[101]={0,};
+
+int arr[101] = {0}; // 바구니 위치 최대값이 100
 int ans = 0;
 
 int main() {
-    int N, K; cin >> N >> K;
-    int size = K*2 + 1; // 범위의 크기
+    int N, K;
+    cin >> N >> K;
+    int size = K * 2 + 1; // 범위의 크기
 
-    for(int i=0; i<N; i++){
+    for (int i = 0; i < N; i++) {
         int n, idx;
         cin >> n >> idx;
-        arr[idx] += n; // 같은 위치에 여러 바구니가 가능한 상황 반영
-        
-    } 
+        arr[idx] += n; // 입력되지 않은 좌표는 기본값 0
+    }
 
-    for(int i=0; i<=101-size; i++){
+    // 기존 방식 유지
+    for (int i = 0; i <= 100 - size + 1; i++) { // 탐색 범위는 배열 전체(0~100)
         int sum = 0;
-        for(int j=0; j<size; j++){
+        for (int j = 0; j < size; j++) {
             sum += arr[i + j];
         }
         ans = max(ans, sum);
     }
-    cout << ans;
-    // 여기에 코드를 작성해주세요.
+
+    cout << ans << endl;
     return 0;
 }
