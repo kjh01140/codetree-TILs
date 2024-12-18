e9e4377f0fa4165906a8c5b9586ca26b92f11e08
kjh01140 <kjh011402023@gmail.com>
2024-12-18 19:14:00 +0900

    Title: 개발팀의 능력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241218/개발팀의 능력/the-capabilities-of-the-development-team.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+int arr[5];
+int sum = 0;  // 인수들의 총합
+int ans = 10000000;
+
+int main() {
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){// 첫번째 짝 선정
+            if(i!=j){
+                for(int k=0; k<5; k++){
+                    for(int l=0; l<5; l++){
+                        if(k!=i && k!=j && l!=i && l!=j && k!=l){
+
+                            int sum1 = arr[i] + arr[j];
+                            int sum2 = arr[k] + arr[l];
+                            int sum3 = sum - sum1 - sum2;
+
+                            if(sum1 != sum2 && sum2 != sum3 && sum3 != sum1){
+                                int big = max({sum1, sum2, sum3});
+                                int small = min({sum1, sum2, sum3});
+                                int diff = big - small;
+
+                                ans = min(ans, diff);
+                            } else{
+                                ans = -1;
+                            }
+                            // 세 팀 합 중 최대 - 최소 계산
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
