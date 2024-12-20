422d2b7df5e46b569627259ca7df8a654240a17a
kjh01140 <kjh011402023@gmail.com>
2024-12-20 22:02:55 +0900

    Title: 스승의 은혜 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241220/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ 241220/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -11,35 +11,28 @@ int main() {
         cin >> price[i];
     }
 
-    
-    
-        
-        for(int i=0; i<N; i++){ // 할인하는 학생 선정
-            int cnt=0;
-            
-                int money = B;
-                cnt = 0;
-                for(int j=0; j<N; j++){
+    // 순서에 구애받지 않고 최대 인원에게 주기 위해 정렬
+    sort(price, price+N);
 
-                    if(j==i){
-                        money-=price[j]/2;
-                        cnt++; // 선물받은 학생수                
-                    } else{                    
-                        money-=price[j];
-                        cnt++; // 선물받은 학생수
-                    }
-
-                    if(money<0){
-                        cnt--;
-                        break;
-                    }
-
-             
-                
+    for(int i=0; i<N; i++){ // 할인하는 학생 선정
+        int cnt=0;
+        int money = B;
+        for(int j=0; j<N; j++){
+            if(j==i){
+                money-=price[j]/2;
+                cnt++; // 선물받은 학생수                
+            } else{                    
+                money-=price[j];
+                cnt++; // 선물받은 학생수
             }
-            ans = max(ans, cnt);
+
+            if(money<0){
+                cnt--;
+                break;
+            }
         }
-    
+        ans = max(ans, cnt);
+    }
     
     cout << ans;
     // 여기에 코드를 작성해주세요.
