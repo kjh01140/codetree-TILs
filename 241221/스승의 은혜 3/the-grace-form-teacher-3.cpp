42bc6c399224475e511df3e59321c7d87e45e330
kjh01140 <kjh011402023@gmail.com>
2024-12-21 18:36:00 +0900

    Title: 스승의 은혜 3, Time: 21ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241221/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241221/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -31,29 +31,26 @@ int main() {
     }
 
     // 정렬: 전체 비용 기준
-    sort(presents, presents + N, cmp);
+    
 
     // 모든 선물을 할인 대상으로 고려
     for (int i = 0; i < N; i++) { // `i`는 할인 대상
         int cnt = 0;     // 선물받은 학생 수
         int money = B;   // 남은 예산
 
-        for (int j = 0; j < N; j++) {
-            if (j == i) { // `i`번째 선물을 할인 적용
-                money -= (presents[j].price / 2) + presents[j].deliver;
-            } else { // 나머지 선물은 원래 가격
-                money -= presents[j].price + presents[j].deliver;
-            }
+        presents[i].price /=2; //할인대상 미리 가격 조정 
+        sort(presents, presents+N, cmp);
 
-            // 예산 초과 시 중단
+        for(int j=0; j<N; j++){
+            money -= presents[j].price + presents[j].deliver;
             if (money < 0) break;
 
-            // 선물받은 학생 수 증가
             cnt++;
         }
 
         // 최대값 갱신
         ans = max(ans, cnt);
+        presents[i].price *=2; //할인대상 기존 가격으로 조정 
     }
 
     cout << ans << endl;
