d851d197c509d72c6861f5d8d1e3203eaa8da7db
kjh01140 <kjh011402023@gmail.com>
2024-12-21 12:31:30 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241221/상해버린 치즈/rotten-cheese.cpp
+++ 241221/상해버린 치즈/rotten-cheese.cpp
@@ -1,55 +1,62 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int eat_record[1000][3] = {};
-int sick_time[50][2] = {};
-int ans = 0;
+int eat_record[1000][3]; // [사람, 치즈, 시간]
+int sick_time[50][2];    // [사람, 아픈 시간]
 
 int main() {
     int N, M, D, S;
     cin >> N >> M >> D >> S;
 
-    for (int i = 1; i <= D; i++)
-        cin >> eat_record[i][0] >> eat_record[i][1] >> eat_record[i][2]; // 사람p, 치즈m, 시간t
+    for (int i = 0; i < D; i++) {
+        cin >> eat_record[i][0] >> eat_record[i][1] >> eat_record[i][2];
+    }
 
-    for (int i = 1; i <= S; i++)
-        cin >> sick_time[i][0] >> sick_time[i][1]; // 아픈사람p, 확실히 아픈 시간t
+    for (int i = 0; i < S; i++) {
+        cin >> sick_time[i][0] >> sick_time[i][1];
+    }
 
-    for (int i = 1; i <= M; i++) { // 1번 치즈부터 M번 치즈까지 상했을 가능성을 판단
-        bool is_possible_cheese = true;
+    int max_people = 0; // 약이 필요한 최대 인원 수
 
-        for (int k = 1; k <= S; k++) { // 각 아픈 사람 확인
-            int person = sick_time[k][0];
-            int time_sick = sick_time[k][1];
+    // 각 치즈를 순회하며 확인
+    for (int cheese = 1; cheese <= M; cheese++) {
+        bool is_possible = true;
+
+        // 모든 아픈 사람에 대해 확인
+        for (int s = 0; s < S; s++) {
+            int sick_person = sick_time[s][0];
+            int sick_time_s = sick_time[s][1];
             bool ate_before_sick = false;
 
-            for (int j = 1; j <= D; j++) { // 치즈를 먹은 기록 확인
-                if (eat_record[j][1] == i && eat_record[j][0] == person) {
-                    if (eat_record[j][2] < time_sick) { // 아프기 전에 먹은 기록이 있으면
+            // 해당 아픈 사람이 치즈를 먹은 기록 확인
+            for (int d = 0; d < D; d++) {
+                if (eat_record[d][1] == cheese && eat_record[d][0] == sick_person) {
+                    if (eat_record[d][2] < sick_time_s) {
                         ate_before_sick = true;
-                    } else { // 아프기 후에 먹었다면 상한 치즈 불가능
-                        is_possible_cheese = false;
-                        break;
                     }
                 }
             }
 
-            if (!ate_before_sick) { // 아프기 전에 먹은 기록이 없다면 상한 치즈 불가능
-                is_possible_cheese = false;
+            // 아프기 전에 치즈를 먹은 기록이 없으면 불가능
+            if (!ate_before_sick) {
+                is_possible = false;
                 break;
             }
         }
 
-        if (is_possible_cheese) { // 가능한 치즈라면 먹은 사람 수 계산
-            int cnt = 0;
-            for (int j = 1; j <= D; j++) {
-                if (eat_record[j][1] == i) cnt++;
+        if (is_possible) {
+            // 가능한 치즈라면 먹은 사람 수 계산
+            int count = 0;
+            for (int d = 0; d < D; d++) {
+                if (eat_record[d][1] == cheese) count++;
             }
-            ans = max(ans, cnt); // 최대값 갱신
+            max_people = max(max_people, count);
         }
     }
 
-    cout << ans;
+    cout << max_people << endl;
+
     return 0;
 }
