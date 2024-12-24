d2c553f023b7b2f7b15ceefee3ce9be7a6e7cc41
kjh01140 <kjh011402023@gmail.com>
2024-12-24 10:45:06 +0900

    Title: 야바위, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241224/야바위/ya-rock.cpp
+++ 241224/야바위/ya-rock.cpp
@@ -3,27 +3,31 @@
 using namespace std;
 
 int Cup[4] = {0, 0, 0, 0};// 0번,,, 1번,2번,3번
+int arr[100][3]={};
 
 int ans = 0;
 int main() {
     int N; cin >> N;
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+    }
+
     for(int i=1; i<=3; i++){ // i는 처음 조약돌을 넣은 위치
         int score = 0;
         int Cup[4] = {0, 0, 0, 0};// 0번,,, 1번,2번,3번
         Cup[i] = 1;
 
-        for(int j=0; j<N; j++){
-            int a, b, c; 
-            cin >> a >> b >> c;
+        for(int j=0; j<N; j++){ //N번 시행
+            int temp = Cup[arr[j][1]];
+            Cup[arr[j][1]] = Cup[arr[j][0]];
+            Cup[arr[j][0]] = temp;
 
-            int temp = Cup[b];
-            Cup[b] = Cup[a];
-            Cup[a] = temp;
-
-            if(Cup[c] == 1) score++;
+            if(Cup[arr[j][2]] == 1) score++;
         }
         
         ans = max(ans , score);
+
     }
 
     cout << ans;
